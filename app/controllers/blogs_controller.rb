3c667a5e14bca726052570d612137adf4f4ec7cf
GitHubのユーザ名 <GitHub>
2020-12-07 12:52:30 +0000

    [Finish]3-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -14,12 +14,19 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    
+    redirect_to blog_path(blog.id)
   end
 
   def edit
+    @blog = Blog.find(params[:id])
   end
-
+  
+    def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+  
    private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
